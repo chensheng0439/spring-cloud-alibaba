8bf7e08dc4c8f1b0fec6f53c78e1b13437b9b778
schen@superv.com.cn <schen@superv.com.cn>
2021-03-26 16:47:35 +0800

    11111

---
+++ spring-cloud-gateway/src/main/java/org/example/GatewayApp.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class GatewayApp {
+    public static void main( String[] args ) {
+        SpringApplication.run(GatewayApp.class,args);
+    }
+}
