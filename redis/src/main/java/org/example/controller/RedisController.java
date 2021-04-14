bf696a5bac0bc00d92083aabdcb1ecb656769f8c
schen@superv.com.cn <schen@superv.com.cn>
2021-04-14 17:10:06 +0800

    11111

---
+++ redis/src/main/java/org/example/controller/RedisController.java
@@ -0,0 +1,22 @@
+package org.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+public class RedisController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("/hello")
+    public String hello(){
+        Set<String> keys = redisTemplate.keys("*");
+        return "hello";
+    }
+
+}
